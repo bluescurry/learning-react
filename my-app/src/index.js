2c95c8f069a4233c2ce780c828230d3642615757
bluescurry <18832643723@163.com>
2021-03-23 23:58:45 +0800

    新建react项目

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <div id="abc">
+  <p>这是一个P标签</p>
+</div>  ,
+  document.getElementById('root')
+);
